80fb513ae79332dd3928f42efbd29f4577a74834
Ruben Imhoff <31476760+RubenImhoff@users.noreply.github.com>
2026-05-07 17:14:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,35 +31,6 @@ common_include = [numpy.get_include()]
 if sys.platform.startswith("darwin"):
     common_link_args.append("-Wl,-rpath,/usr/local/opt/gcc/lib/gcc/9/")
 
-extensions_data = {
-    "pysteps.motion._proesmans": {"sources": ["pysteps/motion/_proesmans.pyx"]},
-    "pysteps.motion._vet": {"sources": ["pysteps/motion/_vet.pyx"]},
-}
-
-extensions = []
-
-for name, data in extensions_data.items():
-    include = data.get("include", common_include)
-
-    extra_compile_args = data.get("extra_compile_args", common_compile_args)
-
-    extra_link_args = data.get("extra_link_args", common_link_args)
-
-    pysteps_extension = Extension(
-        name,
-        sources=data["sources"],
-        depends=data.get("depends", []),
-        include_dirs=include,
-        language=data.get("language", "c"),
-        define_macros=data.get("macros", []),
-        extra_compile_args=extra_compile_args,
-        extra_link_args=extra_link_args,
-    )
-
-    extensions.append(pysteps_extension)
-
-external_modules = cythonize(extensions, force=True, language_level=3)
-
 requirements = [
     "numpy",
     "jsmin",
